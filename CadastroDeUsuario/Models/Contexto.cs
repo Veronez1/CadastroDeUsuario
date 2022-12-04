9da1cc49ddae3d0ac24bb62b4b4cccb0126c1498
Veronez1 <daviveronez80@gmail.com>
2022-12-04 18:37:56 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroDeUsuario/Models/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CadastroDeUsuario.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuario { get; set; } 
+    }
+}
---
+++ CadastroDeUsuario/Program.cs
@@ -0,0 +1,37 @@
+using CadastroDeUsuario.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System.Data.SqlClient;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Contexto>
+    
+    (options => options.UseSqlServer("Data Source=DESKTOP-53JHLHP;Encrypt=False;Initial Catalog = CadastroDeUsuario; User ID = sa; Password = 1234"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
